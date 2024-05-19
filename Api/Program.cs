dc4cc9048d580374e8217210e21528c85346634e
Marlon <marloncano1997@gmail.com>
2024-05-19 15:31:12 -0500

    se ajusta el programa

--- Api/Program.cs
+++ Api/Program.cs
@@ -28,11 +28,10 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configurar el pipeline de solicitud HTTP
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.ApplyMigration();
 //app.SeedData();
